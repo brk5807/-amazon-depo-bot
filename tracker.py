15874189313ea6bdf4bf098763f2e42d23e95ce8
brk5807 <52216177+brk5807@users.noreply.github.com>
2026-07-29 20:33:02 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from bs4 import BeautifulSoup
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
@@ -8,36 +7,22 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 URL = "https://www.amazon.com.tr/s?i=electronics&srs=44219324031&bbn=44219324031&rh=n%3A12466496031%2Cn%3A44219324031%2Cn%3A13709879031"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Referer": "https://www.amazon.com.tr/"
+    "User-Agent": "Mozilla/5.0"
 }
 
 try:
     response = requests.get(URL, headers=headers, timeout=30)
 
-    debug = (
-        f"HTTP: {response.status_code}\n\n"
-        f"{response.text[:3000]}"
-    )
-
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={
-            "chat_id": CHAT_ID,
-            "text": debug[:4000]
-        },
-        timeout=30
-    )
+    text = f"HTTP: {response.status_code}\n\n{response.text[:500]}"
 
 except Exception as e:
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={
-            "chat_id": CHAT_ID,
-            "text": f"HATA:\n{e}"
-        },
-        timeout=30
-    )
-    # test
+    text = f"HATA:\n{e}"
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    json={
+        "chat_id": CHAT_ID,
+        "text": text
+    },
+    timeout=30
+)
