07366466c5dec14d03c1961820d9fe2a4a62b619
brk5807 <52216177+brk5807@users.noreply.github.com>
2026-07-29 17:17:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-print("Amazon Depo Bot Başladı")
+
+import os
+
+print("🚀 Amazon Depo Bot Başladı")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+if BOT_TOKEN:
+    print("✅ Telegram Token bulundu")
+else:
+    print("❌ Telegram Token bulunamadı")
+
+if CHAT_ID:
+    print("✅ Chat ID bulundu")
+else:
+    print("❌ Chat ID bulunamadı")
