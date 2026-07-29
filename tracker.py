aa5c15068865a32dd4a08e6420b36a0a743e3c21
brk5807 <52216177+brk5807@users.noreply.github.com>
2026-07-29 17:35:17 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,22 @@
 
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(
+        url,
+        json={
+            "chat_id": CHAT_ID,
+            "text": text
+        },
+        timeout=15
+    )
+
+
+if __name__ == "__main__":
+    send_message("✅ Amazon Depo Bot başarıyla çalıştı!")
